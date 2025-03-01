ce740ed85a120e497038621b3e2933044ec9c2c4
vishnumur777 <varunushamurali@gmail.com>
2025-03-01 15:35:59 +0530

    chore: add analyze in src/mybatop

---
+++ src/mybatop/analyze/pythonscripts/clean_dataset.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import os
+
+
+df1 = pd.read_csv("data.csv")
+
+mask = df1["SERIAL_NUMBER"].isnull()
+
+cols = df1.columns.tolist()
+
+curr_idx = cols.index('CURRENT_NOW')
+
+col1 = df1.columns[:curr_idx]
+col2 = df1.columns[curr_idx:]
+
+df1.SERIAL_NUMBER = df1.SERIAL_NUMBER.astype(str)
+
+df1.loc[mask, col2] = df1.loc[mask, col2].shift(axis=1)
+
+df1.loc[mask, "CURRENT_NOW"] = df1.loc[mask, "CURRENT_NOW"].fillna(0)
+
+# Save the modified dataframe (optional)
+df1.to_csv("final_fixed.csv", index=False)
+
+os.system("rm -rf data.csv")
+
+os.system("mv final_fixed.csv data.csv")
