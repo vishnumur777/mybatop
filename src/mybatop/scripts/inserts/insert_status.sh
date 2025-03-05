d907daa14c52dc74b4a4cbe9519d113fb804bb83
vishnumur777 <varunushamurali@gmail.com>
2025-03-05 22:48:04 +0530

    fix: adding low-power mode insertion while changing battery status

--- src/mybatop/scripts/inserts/insert_status.sh
+++ src/mybatop/scripts/inserts/insert_status.sh
@@ -2,14 +2,19 @@
 
 read -r prev_status < /sys/class/power_supply/BAT0/status
 
+extract_state() {
+  cat /proc/acpi/button/lid/LID0/state | tr -s ' ' ',' | cut -d ',' -f 2
+}
+
 insert_status() {
-    if [ "$state_read" != "Not charging" ] && [ "$state_read" != "Unknown" ]
+    if [ "$state_read" != "Not charging" ] && [ "$state_read" != "Not-charging" ] && [ "$state_read" != "Unknown" ] && [ "$battery_state" == "closed" ]
     then
 	/opt/mybatop/scripts/inserts/insert_active.sh
     fi
 }
 
 while true; do
+   battery_status=$(extract_state)
    read -r state_read < /sys/class/power_supply/BAT0/status
    if [[ "$state_read" != "$prev_status" ]]; then
        	insert_status
